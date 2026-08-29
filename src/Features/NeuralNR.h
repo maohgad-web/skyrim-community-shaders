b8a2166dc98efab7d607ba9e6eb6ec25a515ea33
maohgad-web <maohgad@gmail.com>
2026-08-29 13:06:41 -0500

    MAGUMBOS

--- src/Features/NeuralNR.h
+++ src/Features/NeuralNR.h
@@ -22,7 +22,7 @@ struct NeuralNR : public Feature
 
 	// Non-static: allows access to settings and private member methods
 	void OnPresent();
-	bool IsEnabled() { return GetState().initialized; }
+	bool IsEnabled() override { return settings.enabled; }
 
 private:
 	struct Settings
